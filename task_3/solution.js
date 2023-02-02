ef93fedeb343388244be4e4d3ed5d393a1cbd234
Alexs37 <alexs37@gmail.com>
2023-02-02 23:06:28 +0300

    Выполнел заданеи 3 автотест пройден успешно.

--- task_3/solution.js
+++ task_3/solution.js
@@ -1,6 +1,6 @@
 function renderCartItem(item) {
     const product = item;
-
+    //let product={id: 1, title: 'Пицца', count: 5, price: '500.00'}
     // Задание №3.1. Формирование строки корзины
 
     // product - объект вида {id: id, title: title, count: count, price: price}
@@ -23,6 +23,8 @@ function renderCartItem(item) {
     // 7. Присвойте переменной itemCountText результат склеивания itemCountText и sum
     // 8. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽`
 
+    let itemCountText=product.count+' × '+ `${product.price} ₽`+' = '+ product.count*product.price+' ₽'
+   
     // Конец решения задания №3.1.
 
     return `<div class="cart-item"><div>${product.title}</div><div>${itemCountText}</div></div>`;
