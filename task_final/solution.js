520ecbcb96336848a070ceaaefca0bf7329f214f
Alexs37 <alexs37@gmail.com>
2023-02-06 01:17:41 +0300

    Выполнел финальное задание

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,18 +34,17 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+   let data = {data: {client: '', order: {address: '', sum}, goods: []}};
+   let countOfGoods = goods.length;
 
-    let countOfGoods = goods.length;
-
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+  for (let i = 0; i < countOfGoods; i += 1) {
+    //     data.goods.push(goods[i].title);
+        data.data.goods[i]=({title: goods[i].title, count: goods[i].count});
     }
-
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
+    data.data.client=name + " " + phone;
+    data.data.order.address= `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв. ${address.flat}`;
+    data.data.order.sum = sum;
+   
 
     let jsonData = JSON.stringify(data);
 
